348596b937ad75e6876b98810ede9b2ebe044090
Artem Travlo <info.travlo@gmail.com>
2022-07-26 12:26:43 +0300

    inital implementation of buble sorting

--- build/index.js
+++ build/index.js
@@ -1,2 +1,22 @@
 "use strict";
-console.log('hi theree');
+class Sorter {
+    collection;
+    constructor(collection) {
+        this.collection = collection;
+    }
+    sort() {
+        const { length } = this.collection;
+        for (let index = 0; index < length; index++) {
+            for (let j = 0; j < length - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    const temp = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
