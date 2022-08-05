421b0bb0d9baf6151a474c45775579a027ca6443
Artem Travlo <info.travlo@gmail.com>
2022-08-05 13:42:13 +0300

    Making my own js framework

--- build/Countable.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Countable = void 0;
-class Countable {
-}
-exports.Countable = Countable;
--- src/Countable.ts
+++
@@ -1,8 +0,0 @@
-export abstract class Countable {
-  abstract data: string[][]
-  abstract team: string
-  abstract homeWin: number | string
-  abstract awayWin: number | string
-  abstract draw: number | string
-  abstract count: number
-}
